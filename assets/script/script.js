49ee20353a639555384d412c5f2d892405671655
Tabitha Buder <tabibuder@gmail.com>
2024-02-13 02:56:55 -0800

    added questions

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,32 @@
-function countdown() {
+let questions = [
+    {
+        question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+        answer: '',
+        correctAnswer: '',
+    },
+    {
+        question: '',
+        answer: '',
+        correctAnswer: '',
+    },
+    {
+        question: '',
+        answer: '',
+        correctAnswer: '',
+    },
+    {
+        question: '',
+        answer: '',
+        correctAnswer: '',
+    },
+    {
+        question: '',
+        answer: '',
+        correctAnswer: '',
+    },
+]
+
+function timer() {
     var timeLeft = 60;
 
     var timeInterval = setInterval(function() {
