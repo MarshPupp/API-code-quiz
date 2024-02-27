fab44aa618d564cb90fd84c2575f6491a49e396e
Tabitha Buder <tabibuder@gmail.com>
2024-02-27 02:45:40 -0800

    added code to log to local storage

--- assets/script/script.js
+++ assets/script/script.js
@@ -28,7 +28,10 @@ const questions = [
 
 let currentQuestionIndex = 0;
 let score = 0;
+let timeLeft = 75;
+let timeInterval;
 
+const timerEL = document.getElementById('timer');
 const quizContainer = document.getElementById('quiz-container');
 const questionContainer = document.getElementById('questions-container');
 const questionText = document.getElementById('question-text');
@@ -36,6 +39,11 @@ const answerMarker = document.getElementById('answer-marker');
 const resultContainer = document.getElementById('result-container');
 const resultText = document.getElementById('result-text');
 const endContainer = document.getElementById('end-container');
+const scoreText = document.getElementById('score-text');
+const saveForm = document.getElementById('save-form');
+const saveBtn = document.getElementById('save-button');
+const scores = JSON.parse(localStorage.getItem('scores')) || [];
+const scoresContainer = document.getElementById('scores-container');
 
 window.onload = function() {
     startQuiz();
@@ -70,24 +78,56 @@ function checkAnswer(selectedAnswer, correctAnswer) {
     if (currentQuestionIndex < questions.length) {
         showQuestion();
     } else {
+        clearInterval(timeInterval);
         endQuiz();
     }
 }
 
 function startTimer() {
-    var timeLeft = 60;
-
-    var timeInterval = setInterval(function() {
+    timeInterval = setInterval(function() {
         if (timeLeft >= 1) {
-            timer.textContent = 'timer:' + timeLeft;
+            timerEL.textContent = 'timer:' + timeLeft;
             timeLeft--;
         } else {
+            clearInterval(timeInterval);
             endQuiz();
         }
     }, 1000);
 }
 
+function updateScore() {
+    scoreText.textContent = 'score:' + score;
+}
+
 function endQuiz() {
     quizContainer.style.display = 'none';
     endContainer.style.display = 'block';
+    updateScore()
 }
+
+saveBtn.addEventListener('click', saveScoreToLocal);
+
+function saveScoreToLocal() {
+    const initialsInput = document.getElementById('initials').value.trim().toUpperCase();
+    if (/^[A-Z]{3}$/.test(initialsInput)) {
+        scores.push({ initials: initialsInput, score });
+        localStorage.setItem('scores', JSON.stringify(scores));
+    } else {
+        console.log("Invalid initials. Please enter exactly 3 letters.");
+    }
+}
+
+function displayScores() {
+    scores.sort((a,b) => b.score - a.score);
+
+    let tableHtml = '<table>';
+    tableHtml += '<tr><th>Rank</th><th>Initials</th><th>Score</th></tr>';
+    scores.forEach((entry, index) => {
+        tableHtml += `<tr><td>${index + 1}</td><td>${entry.initials}</td><td>${entry.score}</td></tr>`;
+    });
+    tableHtml += '</table>';
+
+    document.body.innerHTML = tableHtml;
+
+    scoresContainer.innerHTML = tableHtml;
+};
